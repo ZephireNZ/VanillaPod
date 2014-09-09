1f19388b09751d4473b2d339cb50df41a0e8010e
ZephireNZ <brynleymcdonald@gmail.com>
2014-09-09 14:16:38 +1200

    Remove ping caching - Requires at least build #995 with Async ping

--- src/main/java/nz/co/noirland/vanillapod/VanillaPod.java
+++ src/main/java/nz/co/noirland/vanillapod/VanillaPod.java
@@ -1,26 +1,18 @@
 package nz.co.noirland.vanillapod;
 
+import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.event.ProxyPingEvent;
-import net.md_5.bungee.api.event.ServerConnectEvent;
-import net.md_5.bungee.api.event.ServerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Exchanger;
 
 public class VanillaPod extends Plugin implements Listener {
 
     private static VanillaPod inst;
-
-    private Map<ServerInfo, ServerPing> cachedPings = new ConcurrentHashMap<>(); // Cached server pings
-    private Set<ServerInfo> protoServers; // All the servers that need to be cached
 
     public static VanillaPod inst() {
         return inst;
@@ -33,17 +25,6 @@ public class VanillaPod extends Plugin implements Listener {
 
         getProxy().setReconnectHandler(new ProtocolReconnectManager());
         getProxy().getPluginManager().registerListener(this, this);
-
-        HashSet<ServerInfo> servers = new HashSet<>();
-        for(String server : PodConfig.inst().getServers()) {
-            ServerInfo info = getProxy().getServerInfo(server);
-            servers.add(info);
-        }
-        protoServers = Collections.unmodifiableSet(servers);
-
-        for(ServerInfo server : protoServers) {
-            PingCacheTask.get(server).runTimer(PodConfig.inst().getPingCacheDelay());
-        }
     }
 
     /**
@@ -56,29 +37,28 @@ public class VanillaPod extends Plugin implements Listener {
         int protocol = event.getConnection().getVersion();
         ServerInfo info = getServer(protocol);
         if(info == null) return;
-        event.setResponse(getPing(info));
-    }
 
-    /**
-     * Force refresh the MOTD when a player leaves watched server.
-     */
-    @EventHandler
-    public void onDisconnect(ServerDisconnectEvent event) {
-        ServerInfo left = event.getTarget();
-        if(!protoServers.contains(left)) return;
+        final Exchanger<ServerPing> exch = new Exchanger<>();
 
-        PingCacheTask.get(left).runNow();
-    }
+        // Get the child server's ping, pass it to back to main ping.
+        info.ping(new Callback<ServerPing>() {
+            @Override
+            public void done(ServerPing result, Throwable error) {
+                try {
+                    exch.exchange(result);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-    /**
-     * Force refresh the MOTD when a player joins watched server.
-     */
-    @EventHandler
-    public void onConnect(ServerConnectEvent event) {
-        ServerInfo joined = event.getTarget();
-        if(!protoServers.contains(joined)) return;
-
-        PingCacheTask.get(joined).runNow();
+        try {
+            ServerPing ping = exch.exchange(null); // Wait for child ping thread to finish
+            if(ping == null) return;
+            event.setResponse(ping);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -89,23 +69,5 @@ public class VanillaPod extends Plugin implements Listener {
     public ServerInfo getServer(int protocol) {
         String name = PodConfig.inst().getServer(protocol);
         return getProxy().getServerInfo(name);
-    }
-
-    /**
-     * Get the cached ping for this server
-     * @param server Server to get
-     * @return A cached ping
-     */
-    public ServerPing getPing(ServerInfo server) {
-        return cachedPings.get(server);
-    }
-
-    /**
-     * Sets the cached ping for the given server
-     * @param server Server's info
-     * @param ping Ping to cache
-     */
-    public void setPing(ServerInfo server, ServerPing ping) {
-        cachedPings.put(server, ping);
     }
 }
